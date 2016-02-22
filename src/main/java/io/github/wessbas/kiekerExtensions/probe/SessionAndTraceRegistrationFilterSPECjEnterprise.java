ea6d526a384c3132d6a46df1cb1ec1ec3ba48e23
Jonas Heinisch <jonas@heinisch-web.de>
2016-02-22 22:47:32 +0100

    changed to new header

--- src/main/java/io/github/wessbas/kiekerExtensions/probe/SessionAndTraceRegistrationFilterSPECjEnterprise.java
+++ src/main/java/io/github/wessbas/kiekerExtensions/probe/SessionAndTraceRegistrationFilterSPECjEnterprise.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Copyright 2015 Kieker Project (http://kieker-monitoring.net)
+ * Copyright (c) 2016 the WESSBAS project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************/
+
+
 
 package io.github.wessbas.kiekerExtensions.probe;
 
